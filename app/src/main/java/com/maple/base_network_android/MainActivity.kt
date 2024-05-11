2e6688b93c1a5d878a412f4240a4b459dd9eaf7c
shangyf <shangyf0515@126.com>
2024-05-11 17:18:17 +0800

    首次提交

---
+++ app/src/main/java/com/maple/base_network_android/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.maple.base_network_android
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
